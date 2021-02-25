2b58437f5d6da520f421b60abae502645a76b1b4
Mia8989 <ms.mia.knight@gmail.com>
2021-02-25 12:02:48 -0600

    E-commerce website 1st commit

---
+++ src/main/java/com/tts/ecomspring/model/Product.java
@@ -0,0 +1,33 @@
+package com.tts.ecomspring.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO);
+    @Column(name='product_id')
+
+//ints
+    private int id;
+    private int quantity;
+    private int price;
+
+
+
+//strings
+    private String brand;
+    private String category;
+    private String name;
+    private String image;
+    private String description;
+
+
+
+}
---
+++ src/main/java/com/tts/ecomspring/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.tts.ecomspring.repository;
+
+import org.apache.catalina.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByUsername(String username);
+
+}
