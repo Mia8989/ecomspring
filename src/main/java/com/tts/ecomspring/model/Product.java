85b532cbb84f60c78cfd3b516e12074b2fee1c75
Mia8989 <ms.mia.knight@gmail.com>
2021-02-25 19:11:16 -0600

    E-commerce website 3rd commit resolved product.java in model issue

--- src/main/java/com/tts/ecomspring/model/Product.java
+++ src/main/java/com/tts/ecomspring/model/Product.java
@@ -6,9 +6,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
 
 @Builder
 @Data
@@ -17,7 +19,7 @@ import javax.persistence.Id;
 @Entity
 public class Product {
     @Id
-    @GeneratedValue (strategy = GenerationType.AUTO);
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
 //ints
